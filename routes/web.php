546a1014e1493b942e4457d95590a929103e5355
devendi <endi.efendi@gmail.com>
2022-05-04 07:19:19 +0700

    directive blade @yield @extends @section

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+
+Route::get('/home', function () {
+    return view('home', [
+        'title' => 'Home'
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
+        'title' => 'About',
         'name' => 'Endi Efendi',
         'email' => 'endi.efendi@gmail.com',
         'image' => 'efendi.png'
@@ -26,5 +35,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        'title' => 'Posts'
+    ]);
 });
