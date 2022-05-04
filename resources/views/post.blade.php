0043948a17497c1fc6d738455a8614d5cf019bd6
devendi <endi.efendi@gmail.com>
2022-05-04 08:27:58 +0700

    take data dari routes dan ditampilkan di view dengan sistem template blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,10 @@
 @extends('layouts.main')
 {{-- dumb die @dd($posts) --}}
 @section('container')
-
-
-@foreach ($posts as $post)
     <article class="mb-5">
-        <h2>
-            <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
-        </h2>
+        <h2>{{ $post['title'] }}</h2>
         <h5>By : {{ $post['author'] }}</h5>
         <p>{{ $post['body'] }}</p>
+        <a href="/blog">back</a>
     </article>
-@endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,71 @@ Route::get('/about', function () {
     ]);
 });
 
+
+
 Route::get('/blog', function () {
+        
+    $blogPost = [
+        [
+            'title' => 'Judul Post Pertama',
+            'slug' => 'Judul-Post-Pertama',
+            'author' => 'Endi Efendi',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Sunt a dignissimos nam quidem ratione quam at facere itaque neque officia. 
+            Repellendus possimus facilis illum fugit earum! Saepe ipsum porro facere.',
+        ],
+        [
+            'title' => 'Judul Post Kedua',
+            'slug' => 'Judul-Post-Kedua',
+            'author' => 'Qori',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Doloremque deleniti animi fuga repellat ut aspernatur, sit eaque? 
+            Officia, delectus facere excepturi eum fugit illo molestias quo explicabo asperiores mollitia dicta in, quaerat provident iure repellendus porro! 
+            Quis iure ipsum delectus qui sed molestias perferendis iusto similique repellat quidem, quas quaerat, facilis necessitatibus praesentium est exercitationem nulla officia corporis pariatur, eum adipisci alias molestiae repudiandae. 
+            Exercitationem non neque similique pariatur eligendi laudantium vitae, eius voluptate nobis illo tenetur facilis distinctio perspiciatis eum sint libero ex doloribus nostrum placeat voluptatibus vero, facere, vel nihil corporis. 
+            Amet eos nesciunt porro? Perferendis, deleniti tempora.',
+        ],
+    ];
+
     return view('posts', [
-        'title' => 'Posts'
+        'title' => 'Posts',
+        'posts' => $blogPost
+    ]);
+});
+
+
+Route::get('/posts/{slug}', function ($slug) {
+    
+    $blogPost = [
+        [
+            'title' => 'Judul Post Pertama',
+            'slug' => 'Judul-Post-Pertama',
+            'author' => 'Endi Efendi',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Sunt a dignissimos nam quidem ratione quam at facere itaque neque officia. 
+            Repellendus possimus facilis illum fugit earum! Saepe ipsum porro facere.',
+        ],
+        [
+            'title' => 'Judul Post Kedua',
+            'slug' => 'Judul-Post-Kedua',
+            'author' => 'Qori',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Doloremque deleniti animi fuga repellat ut aspernatur, sit eaque? 
+            Officia, delectus facere excepturi eum fugit illo molestias quo explicabo asperiores mollitia dicta in, quaerat provident iure repellendus porro! 
+            Quis iure ipsum delectus qui sed molestias perferendis iusto similique repellat quidem, quas quaerat, facilis necessitatibus praesentium est exercitationem nulla officia corporis pariatur, eum adipisci alias molestiae repudiandae. 
+            Exercitationem non neque similique pariatur eligendi laudantium vitae, eius voluptate nobis illo tenetur facilis distinctio perspiciatis eum sint libero ex doloribus nostrum placeat voluptatibus vero, facere, vel nihil corporis. 
+            Amet eos nesciunt porro? Perferendis, deleniti tempora.',
+        ],
+    ];
+    $new_post = [];
+    foreach ($blogPost as $post){
+        if($post['slug']  === $slug){
+            $new_post = $post;
+        }
+    };
+
+    return view('post', [
+        'title' => 'single post',
+        'post' => $new_post
     ]);
 });
